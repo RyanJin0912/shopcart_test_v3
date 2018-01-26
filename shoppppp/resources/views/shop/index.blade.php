28da4b0143a5acf8d3e87ae43f1b2c6691c8b4cd
RyanJin <a0988254326@gmail.com>
2018-01-26 10:54:48 +0800

    3_如何MVC_V與M

---
+++ shoppppp/resources/views/shop/index.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.master')
+
+        @section('title')
+            Laravel Ryan's Shop
+        @endsection
+
+
+
+        @section('content')
+
+            <div class="row">
+
+                <div class="col-sm-6 col-md-4">
+
+                    <div class="thumbnail">
+                        <img src="http://3.bp.blogspot.com/-3REl1dnP4Xs/VJLZohkS0mI/AAAAAAAAEIU/M0Bb4r6XLOs/s1600/laravel-logo.jpg" alt="..."  class="img-responsive">
+
+                        <div class="caption">
+
+                            <h3>Product title(Product 1)</h3>
+                            <p class="description">
+                                fjfdisfonvodfnvohdflhglkfd
+
+                            </p>
+
+                            <div class="clearfix">
+                                <div class="pull-left  price">$1000</div>
+                                <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                            </div>
+
+
+                        </div>      {{--表單--}}
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
+        @endsection
